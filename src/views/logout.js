44200610a1d66d8d3dffecbc55d27f4747e35eb5
vassdeniss <vassdeniss@gmail.com>
2023-04-24 15:37:16 +0300

    Delete local storage regardless of backend state

--- src/views/logout.js
+++ src/views/logout.js
@@ -1,10 +1,11 @@
 import { logout } from '../data/auth.js';
+import { clearUserData } from '../util.js';
 
 export async function logoutView(ctx) {
     try {
         await logout(ctx.user.sessionToken);
     } catch (err) {
-        alert(err.message);
+        clearUserData();
         err.handled = true;
     } finally {
         ctx.page.redirect('/settings');
